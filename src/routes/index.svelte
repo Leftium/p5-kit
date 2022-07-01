30a80fd290797125ec503620b7b286d9aa972bf6
John-Kim <Leftium@users.noreply.github.com>
2022-07-02 00:33:46 +0900

    P5.js + HMR + preserve local state.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    // @hmr:keep-all
+    import { globalOnDemandP5 } from '$lib/global-on-demand-p5.js'
+    globalOnDemandP5({setup, draw});
+
+    // Top level variables are preserved by Svelte.
+    let x;
+
+    function setup() {
+        createCanvas(500, 300);
+        x = x || width/2;
+    }
+
+    function draw() {
+        background(200);
+        x = (x + 1) % width;
+        line(x, 0, x, height);
+    }
+</script>
